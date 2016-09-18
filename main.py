c15f8fc7d1fe1659bc4c44d21b47896b0be81fd2
André Vennberg <andre.vennberg@gmail.com>
2016-09-18 21:04:26 +0200

    Should work fine now

--- main.py
+++ main.py
@@ -1,46 +1,82 @@
 import os
+import re
 import praw
 import requests
 import threading
 
 reddit = None
+subreddit = None
 last_status = False
 clientCallback = 'http://127.0.0.1:65010/authorize_callback'
 
+twitchHeaders = {
+    'Client-ID': os.environ['TWITCH_ID']
+}
+
+rightPaddingRegex = re.compile(r"(twitch\.tv\/SovietWomble\"\] \{(\n.+)+)padding-right: (\d)px;")
+
+liveCss = '\n\n\
+.side .md [href="https://www.twitch.tv/SovietWomble"]::after {\n\
+    content: "LIVE!";\n\
+    border: 3px solid red;\n\
+    background-color: red;\n\
+    margin-left: 10px;\n\
+    animation: live 1s linear infinite;\n\
+}'
+
 
 def init_reddit():
-    try:
-        clientID = os.environ['REDDIT_ID']
-        clientSecret = os.environ['REDDIT_SECRET']
-        refresh_token = os.environ['REFRESH_TOKEN']
-
-    except KeyError:
-        clientID = 'SR84ToXmUjfzfQ'
-        clientSecret = 'SMmzNu3m_QBubneR4-gQb0tX-F8'
-        refresh_token = "62832500-cAD5izvDolVo3krTD6fPhQwu0T0"
+    clientID = os.environ['REDDIT_ID']
+    clientSecret = os.environ['REDDIT_SECRET']
+    refresh_token = os.environ['REFRESH_TOKEN']
     
-    global reddit
+    global reddit, subreddit
     reddit = praw.Reddit('Womble Stream Checker v0.1 by /u/anden3')
+    subreddit = reddit.get_subreddit('sovietwomble')
+    
+    reddit.config.decode_html_entities = True
 
     reddit.set_oauth_app_info(client_id=clientID, client_secret=clientSecret, redirect_uri=clientCallback)
     reddit.refresh_access_information(refresh_token)
 
 
 def get_twitch_status():
-    return requests.get("https://api.twitch.tv/kraken/streams/sovietwomble").json()["stream"] != "null"
+    return requests.get("https://api.twitch.tv/kraken/streams/sovietwomble", headers=twitchHeaders).json()["stream"] != "null"
 
+
+def change_stylesheet(live):
+    global reddit, subreddit
+    stylesheet = subreddit.get_stylesheet()['stylesheet']
+    afterCssExists = stylesheet.find("animation: live") != -1
+    
+    mr = rightPaddingRegex.search(stylesheet)
+    stylesheetList = list(stylesheet)
+    
+    if live:
+        stylesheetList[mr.start(3):mr.end(3)] = "0";
+        stylesheet = ''.join(stylesheetList)
+        
+        if not afterCssExists:
+            stylesheet += liveCss
+    
+    else:
+        stylesheetList[mr.start(3):mr.end(3)] = "4";
+        stylesheet = ''.join(stylesheetList)
+        
+        if afterCssExists:
+            stylesheet = stylesheet[:-len(liveCss)]
+    
+    subreddit.set_stylesheet(stylesheet)
 
 def loop():
-    global reddit, last_status
+    global last_status
     
-    threading.Timer(300.0, loop).start()
+    threading.Timer(10.0, loop).start()
     twitch_status = get_twitch_status()
     
     if twitch_status != last_status:
         last_status = twitch_status
-        
-        stylesheet = reddit.get_stylesheet("sovietwomble")
-        print(stylesheet)
+        change_stylesheet(last_status)
 
 init_reddit()
 loop()
